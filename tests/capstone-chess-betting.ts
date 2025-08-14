6556e7eef421cf761cf725ebac9c3b5a2cf76518
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-08-14 23:58:21 +0530

    tests init

--- tests/capstone-chess-betting.ts
+++ tests/capstone-chess-betting.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CapstoneChessBetting } from "../target/types/capstone_chess_betting";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 describe("capstone-chess-betting", () => {
   
@@ -12,6 +13,12 @@ describe("capstone-chess-betting", () => {
   const connection = provider.connection;
   const wallet = provider.wallet as anchor.Wallet;
 
-  
+  let winner: PublicKey | null = null;
+
+  let seed = new anchor.BN(1);
+  let bet_amount = new anchor.BN(LAMPORTS_PER_SOL * 0.01); // 0.01 SOL
+  let match_duration = new anchor.BN(60);
+  let playerA = Keypair.generate();
+  let playerB = Keypair.generate(); 
 
 });
